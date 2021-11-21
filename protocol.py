728cfe9748f107eb2bb17ceca7f36541dff5d124
Tushar1412 <34719833+Tushar1412@users.noreply.github.com>
2021-11-21 13:18:09 +1300

    Update protocol.py

--- protocol.py
+++ protocol.py
@@ -1,25 +1,44 @@
-class Port:
+from scapy.all import *
+from scapy.layers.inet import TCP, UDP
+
+httpMethodList = [b'HTTP', b'GET', b'HEAD', b'POST', b'PUT', b'DELETE', b'CONNECT', b'OPTIONS', b'TRACE', b'PATCH']
+
+
+def is_http(_packet):
+    if TCP not in _packet:
+        return False
+    if len(_packet[TCP].payload) == 0:
+        return False
+    content = _packet[TCP].load
+    if content and any(map(lambda x: content.startswith(x), httpMethodList)):
+        return True
+    return False
+
+
+class Protocol:
     def __init__(self, text):
-        self.text = text
-        if text == "any":
+        if text not in ['tcp', 'udp', 'http', 'any']:
+            raise ValueError
+        if text == 'any':
             self.any = True
         else:
             self.any = False
-            if ':' in text:
-                start, end = text.split(':')
-                start = 0 if start == '' else int(start)
-                end = 65535 if end == '' else int(end)
-                self.portList = range(start, end + 1)
-            elif ',' in text:
-                self.portList = map(int, text.split(','))
-            else:
-                self.portList = [int(text)]
+        self.protocol = text
 
     def __repr__(self):
         if self.any:
-            return "Port of any"
+            return "Protocol of any"
         else:
-            return "Port of "+self.text
+            return "Protocol of "+self.protocol
 
-    def match(self, port):
-        return self.any or port in self.portList
+    def match(self, _packet):
+        if self.any:
+            return True
+        if self.protocol == "udp" and UDP in _packet:
+            return True
+        if self.protocol == "tcp" and TCP in _packet:
+            return True
+        if self.protocol == "http" and TCP in _packet:
+            if is_http(_packet):
+                return True
+        return False
