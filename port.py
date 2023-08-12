7fb8a9e1a201622e5c9c518de16fc481560613d9
Tushar1412 <34719833+Tushar1412@users.noreply.github.com>
2023-08-12 16:13:39 +1200

    port.py

--- port.py
+++ port.py
@@ -1,25 +1,40 @@
 class Port:
     def __init__(self, text):
         self.text = text
+        self.any = False
+        self.portList = set()
+
         if text == "any":
             self.any = True
         else:
-            self.any = False
-            if ':' in text:
-                start, end = text.split(':')
-                start = 0 if start == '' else int(start)
-                end = 65535 if end == '' else int(end)
-                self.portList = range(start, end + 1)
-            elif ',' in text:
-                self.portList = map(int, text.split(','))
-            else:
-                self.portList = [int(text)]
+            self.parse_ports(text)
 
     def __repr__(self):
         if self.any:
-            return "Port of any"
+            return "Any Port"
         else:
-            return "Port of "+self.text
+            return "Port: " + self.text
+
+    def parse_ports(self, text):
+        """
+        Parse the input text to create a list of port numbers or ranges.
+        """
+        if ':' in text:
+            start, end = map(int, text.split(':'))
+            self.portList.update(range(start, end + 1))
+        elif ',' in text:
+            self.portList.update(map(int, text.split(',')))
+        else:
+            self.portList.add(int(text))
 
     def match(self, port):
+        """
+        Check if the provided port matches the criteria set by the Port object.
+        """
         return self.any or port in self.portList
+
+    def is_nmap_port_scan(self, packet):
+        """
+        Check if the packet indicates an Nmap port scan.
+        """
+        return packet.haslayer(TCP) and (packet[TCP].flags == 2 or packet[TCP].flags == 18)
