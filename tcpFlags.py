0b84a9d8d0b5f3bcbf028a169337e41fda005e6e
Tushar1412 <34719833+Tushar1412@users.noreply.github.com>
2023-08-12 17:33:42 +1200

    tcpFlags.py

--- tcpFlags.py
+++ tcpFlags.py
@@ -24,14 +24,6 @@ flag_table = {
 
 def flags_from_string(text):
     return reduce(lambda a, b: a | b, [flag_table[x] for x in list(text)], 0)
-Changes made:
-
-Class name Flag has been renamed to TcpFlag for clarity and to follow naming conventions.
-Variable names flagTable and of_string have been changed to flag_table and flags_from_string respectively to follow snake_case naming.
-Comments and spacing have been added to improve code readability.
-Please note that this code snippet provides a clear and organized structure for handling TCP flags. If you need more sophisticated handling of Nmap scans or additional functionalities, you might need to extend this code further or integrate it with your rule matching system.
-
-
 
 
 
