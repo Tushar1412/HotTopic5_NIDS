f8180a2b10c8b2a2851086de18b42e1190348b1e
Tushar1412 <34719833+Tushar1412@users.noreply.github.com>
2023-08-12 16:16:09 +1200

    protocol.py

--- protocol.py
+++ protocol.py
@@ -1,44 +1,41 @@
 from scapy.all import *
 from scapy.layers.inet import TCP, UDP
 
-httpMethodList = [b'HTTP', b'GET', b'HEAD', b'POST', b'PUT', b'DELETE', b'CONNECT', b'OPTIONS', b'TRACE', b'PATCH']
-
-
-def is_http(_packet):
-    if TCP not in _packet:
-        return False
-    if len(_packet[TCP].payload) == 0:
-        return False
-    content = _packet[TCP].load
-    if content and any(map(lambda x: content.startswith(x), httpMethodList)):
-        return True
-    return False
-
+# List of HTTP methods to check for
+HTTP_METHODS = [b'HTTP', b'GET', b'HEAD', b'POST', b'PUT', b'DELETE', b'CONNECT', b'OPTIONS', b'TRACE', b'PATCH']
 
 class Protocol:
     def __init__(self, text):
         if text not in ['tcp', 'udp', 'http', 'any']:
-            raise ValueError
+            raise ValueError("Invalid protocol: " + text)
+        
+        self.any = False
+        self.protocol = text
+        
         if text == 'any':
             self.any = True
-        else:
-            self.any = False
-        self.protocol = text
 
     def __repr__(self):
         if self.any:
-            return "Protocol of any"
-        else:
-            return "Protocol of "+self.protocol
+            return "Any Protocol"
+        return "Protocol: " + self.protocol.upper()
 
     def match(self, _packet):
         if self.any:
             return True
-        if self.protocol == "udp" and UDP in _packet:
-            return True
-        if self.protocol == "tcp" and TCP in _packet:
-            return True
-        if self.protocol == "http" and TCP in _packet:
-            if is_http(_packet):
-                return True
+        
+        if self.protocol == "udp":
+            return UDP in _packet
+        elif self.protocol == "tcp":
+            return TCP in _packet
+        elif self.protocol == "http" and TCP in _packet:
+            return is_http(_packet)
+        
         return False
+
+def is_http(_packet):
+    if TCP in _packet and _packet[TCP].payload:
+        content = _packet[TCP].load
+        if any(content.startswith(method) for method in HTTP_METHODS):
+            return True
+    return False
