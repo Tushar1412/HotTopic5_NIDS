326dc51e8a3751a539beca77dca86fad4040c5c2
Tushar1412 <34719833+Tushar1412@users.noreply.github.com>
2023-08-12 16:05:36 +1200

    RuleMaker.py

--- RuleMaker.py
+++ RuleMaker.py
@@ -1,15 +1,28 @@
 from rule import Rule
 
-
 class RuleMaker:
     def __init__(self, file_path):
         self.rules = []
+
+        # Read rules from the specified file
         with open(file_path, 'r') as f:
-            while True:
-                line = f.readline().strip()
-                if not line:
-                    break
-                self.rules.append(Rule(line))
+            for line in f:
+                stripped_line = line.strip()
+                if stripped_line:
+                    self.rules.append(Rule(stripped_line))
 
     def get_rules(self):
+        """
+        Returns the list of rules created by the RuleMaker.
+        """
         return self.rules
+
+# Example usage:
+if __name__ == "__main__":
+    file_path = "rules.txt"  # Provide the path to your rules file
+    rule_maker = RuleMaker(file_path)
+    rules = rule_maker.get_rules()
+
+    print("List of Rules:")
+    for idx, rule in enumerate(rules, start=1):
+        print(f"Rule {idx}: {rule}")
