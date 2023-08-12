09497bbafa018d2cfbbeed9aa436bff35688ec00
Tushar1412 <34719833+Tushar1412@users.noreply.github.com>
2023-08-12 16:24:14 +1200

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-type -P python3 >/dev/null 2>&1 || apt install python3
-python3 -m pip >/dev/null 2>&1 || apt install python3-pip -y
-python3 -c "import netaddr" >/dev/null 2>&1 || sudo -H python3 -m pip install -U netaddr
-python3 -c "from scapy.all import *" >/dev/null 2>&1 || sudo -H python3 -m pip install -U scapy
-echo ".....Successfully installed....."
+# Check if Python3 is installed and install if not
+if ! type -P python3 &>/dev/null; then
+    echo "Python3 is not installed. Installing..."
+    sudo apt update
+    sudo apt install python3
+fi
+
+# Check if Python3 pip is installed and install if not
+if ! python3 -m pip &>/dev/null; then
+    echo "Python3 pip is not installed. Installing..."
+    sudo apt update
+    sudo apt install python3-pip -y
+fi
+
+# Check if netaddr Python library is installed and install if not
+if ! python3 -c "import netaddr" &>/dev/null; then
+    echo "netaddr Python library is not installed. Installing..."
+    sudo -H python3 -m pip install -U netaddr
+fi
+
+# Check if scapy Python library is installed and install if not
+if ! python3 -c "from scapy.all import *" &>/dev/null; then
+    echo "scapy Python library is not installed. Installing..."
+    sudo -H python3 -m pip install -U scapy
+fi
+
+echo "Installation completed successfully."
